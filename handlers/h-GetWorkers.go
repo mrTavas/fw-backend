d9326b091454d147f0b12d52a90ea058e0d0072e
mrTavas <d.tavasiev1997@gmail.com>
2020-05-09 00:24:09 +0300

    Added handler GetWorkers

---
+++ handlers/h-GetWorkers.go
@@ -0,0 +1,30 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo"
+
+	//local
+	db "github.com/mrTavas/fw-backend/dbconn"
+	"github.com/mrTavas/fw-backend/models"
+)
+
+// AllWorkers - json response type
+type AllWorkers struct {
+	Workerss []models.Workers `json:"workers"`
+}
+
+// GetWorkers -  Return all managers from db
+func GetWorkers(c echo.Context) error {
+
+	var OutResponse AllWorkers
+
+	_, err := db.Conn.Query(&OutResponse.Workerss, "SELECT * FROM workers")
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, err)
+	}
+
+	return echo.NewHTTPError(http.StatusOK, OutResponse)
+
+}
