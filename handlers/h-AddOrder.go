ab7c7aeb10383dc992b45eb1fc5806e1ff21c5b6
mrTavas <d.tavasiev1997@gmail.com>
2020-05-23 15:24:49 +0300

    some changes in addOrder handler

--- handlers/h-AddOrder.go
+++ handlers/h-AddOrder.go
@@ -12,16 +12,17 @@ import (
 
 // AddOrder add new worker in table Workers
 // json format:
-//{
-//	"client_initials": "Clientov A.V.",
-//	"client_phone" : 79888563211"
-//	"current_worker_initials": "Ivanon I. I.",
+// {
+//  "status": "Office"
+// 	"client_initials": "Clientov A.V.",
+// 	"client_phone" : 79888563211,
+// 	"current_worker_initials": "Ivanon I. I.",
 //  "current_worker_phone": 7988121212,
 //  "cost_manufacturing": 3000,
 //  "cost_painting": 2000,
 //  "cost_finishing": 1500,
 //  "cost_full": 7500
-//}
+// }
 func AddOrder(c echo.Context) error {
 
 	var inputJSON models.Orders
@@ -42,6 +43,7 @@ func AddOrder(c echo.Context) error {
 		CostPainting:          inputJSON.CostPainting,
 		CostFinishing:         inputJSON.CostFinishing,
 		CostFull:              inputJSON.CostFull,
+		Params:                inputJSON.Params,
 	})
 
 	if err != nil {
