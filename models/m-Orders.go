0acd12cba74f59f3c23cb04201a6b722229edf68
mrTavas <d.tavasiev1997@gmail.com>
2020-05-31 21:59:10 +0300

    update Order's model

--- models/m-Orders.go
+++ models/m-Orders.go
@@ -8,13 +8,31 @@ type OrdersParam struct {
 	Height  int    `json:"height"`
 	Width   int    `json:"width"`
 	Filenka string `json:"filenka"`
+
+	Color        string `json:"color"`
+	Patina       string `json:"patina"`
+	FasadArticle string `json:"fasad_article"`
+	Material     string `json:"material"`
+}
+
+// OrderStatus and Data list for orders
+type OrderStatus struct {
+	DataOffice        time.Time `sql:"default:now()" json:"data_office"`
+	DataManufacturing time.Time `sql:"default:now()" json:"data_manufacturing"`
+	DataGrinding      time.Time `sql:"default:now()" json:"data_grinding  "`
+	DataReady         time.Time `sql:"default:now()" json:"data_ready"`
+
+	StatusOffice        bool `sql:",notnull, default:false" json:"status_office"`
+	StatusManufacturing bool `sql:",notnull, default:false" json:"status_manufacturing"`
+	StatusGrinding      bool `sql:",notnull, default:false" json:"status_grinding"`
+	StatusReady         bool `sql:",notnull, default:false" json:"status_ready"`
 }
 
 // Orders Order's table
 type Orders struct {
-	ID     int       `sql:",pk"`
-	Date   time.Time `sql:"default:now()"`
-	Status string    `sql:",notnull" json:"status"`
+	ID     int         `sql:",pk"`
+	Date   time.Time   `sql:"default:now()"`
+	Status OrderStatus `json:"status"`
 
 	ClientInitials        string `sql:",notnull" json:"client_initials"`
 	ClientPhone           int    `json:"client_phone"`
