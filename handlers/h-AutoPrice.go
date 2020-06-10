6aff61672a15f3af440d2c2cc986f19925edf7dc
mrTavas <d.tavasiev1997@gmail.com>
2020-06-11 00:57:03 +0300

    update autoPrice

--- handlers/h-AutoPrice.go
+++ handlers/h-AutoPrice.go
@@ -13,17 +13,17 @@ import (
 // GetAutoPrice - add prices in db
 func GetAutoPrice() {
 
-	AddAutoPrice("f_01", 4200)
-	AddAutoPrice("f_02", 4500)
-	AddAutoPrice("f_03", 5000)
-	AddAutoPrice("f_04", 5500)
-	AddAutoPrice("a_01", 7000)
-	AddAutoPrice("a_02", 8000)
-	AddAutoPrice("b_01", 7500)
-	AddAutoPrice("Provence", 7000)
-	AddAutoPrice("Modern", 6000)
-	AddAutoPrice("Mausoleum", 6500)
-	AddAutoPrice("Massif", 8500)
+	AddAutoPrice("Ф_01", 4200)
+	AddAutoPrice("Ф_02", 4500)
+	AddAutoPrice("Ф_03", 5000)
+	AddAutoPrice("Ф_04", 5500)
+	AddAutoPrice("А_01", 7000)
+	AddAutoPrice("А_02", 8000)
+	AddAutoPrice("В_01", 7500)
+	AddAutoPrice("Прованс", 7000)
+	AddAutoPrice("Модерн", 6000)
+	AddAutoPrice("Мавзолей", 6500)
+	AddAutoPrice("Массив", 8500)
 
 }
 
