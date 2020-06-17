11016bc46fb51838d146f3bf717350f6738bfa85
mrTavas <d.tavasiev1997@gmail.com>
2020-06-18 00:13:32 +0300

    added get all current worler's oders

---
+++ handlers/h-GetWorkerCurrentOrders.go
@@ -0,0 +1,32 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo"
+
+	// local
+	db "github.com/mrTavas/fw-backend/dbconn"
+)
+
+// GetWorkerCurrentOrders -
+func GetWorkerCurrentOrders(c echo.Context) error {
+
+	var OutResponse AllSavedOrders
+	var inputJSON WorkerID
+
+	err := c.Bind(&inputJSON)
+
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, "Wrong data")
+	}
+
+	err = db.Conn.Model(&OutResponse.SavedOrders).Where("current_worker_id = ?", inputJSON.ID).Select()
+
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, err)
+	}
+
+	return echo.NewHTTPError(http.StatusOK, OutResponse)
+
+}
