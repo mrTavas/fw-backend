b3384fed0eb3becce1258cffa681bcd665406428
mrTavas <d.tavasiev1997@gmail.com>
2020-06-18 00:17:00 +0300

    Added worker's and client's id

--- models/m-Orders.go
+++ models/m-Orders.go
@@ -27,13 +27,45 @@ type OrderStatus struct {
 
 // Orders Order's table
 type Orders struct {
-	ID     int         `sql:",pk"`
+	ID     int         `sql:",pk" json:"id"`
 	Title  string      `json:"title"`
 	Date   time.Time   `sql:"default:now()"`
 	Status OrderStatus `json:"status"`
 
-	ClientInitials        string `sql:",notnull" json:"client_initials"`
-	ClientPhone           int    `json:"client_phone"`
+	ClientID       int    `sql:"default:Null" json:"client_id"`
+	ClientInitials string `sql:",notnull" json:"client_initials"`
+	ClientPhone    int    `json:"client_phone"`
+
+	CurrentWorkerID       int    `sql:",notnull" json:"current_worker_id"`
+	CurrentWorkerInitials string `sql:",notnull" json:"current_worker_initials"`
+	CurrentWorkerPhone    int    `json:"current_worker_phone"`
+
+	Color        string `json:"color"`
+	Patina       string `json:"patina"`
+	FasadArticle string `json:"fasad_article"`
+	Material     string `json:"material"`
+
+	CostManufacturing int `sql:",notnull" json:"cost_manufacturing"`
+	CostPainting      int `sql:",notnull" json:"cost_painting"`
+	CostFinishing     int `sql:",notnull" json:"cost_finishing"`
+	CostFull          int `sql:",notnull" json:"cost_full"`
+
+	Params []OrdersParam `json:"params"`
+}
+
+// SavedOrders - saves orders when status chenged
+type SavedOrders struct {
+	ID      int `sql:",pk"`
+	OrderID int
+	Title   string      `json:"title"`
+	Date    time.Time   `sql:"default:now()"`
+	Status  OrderStatus `json:"status"`
+
+	ClientID       int    `sql:"default:Null"`
+	ClientInitials string `sql:",notnull" json:"client_initials"`
+	ClientPhone    int    `json:"client_phone"`
+
+	CurrentWorkerID       int    `sql:",notnull"`
 	CurrentWorkerInitials string `sql:",notnull" json:"current_worker_initials"`
 	CurrentWorkerPhone    int    `json:"current_worker_phone"`
 
