d2588671ee34181b53d39c4f17c9f831bb6574f4
mrTavas <d.tavasiev1997@gmail.com>
2020-06-18 00:15:59 +0300

    Now this method save order in saved orders and change current workers and statuses in current order

--- handlers/h-NextStatus.go
+++ handlers/h-NextStatus.go
@@ -11,22 +11,39 @@ import (
 	"github.com/mrTavas/fw-backend/models"
 )
 
+type nextStatus struct {
+	OrderID     int `json:"order_id"`
+	NewWorderID int `json:"new_worker_id"`
+}
+
 // NextStatus Return
 func NextStatus(c echo.Context) error {
 
-	var inputJSON OrderID
+	var inputJSON nextStatus
 	var statuses models.Orders
 	var currentStatus string
+
+	var worker models.Workers
 
 	err := c.Bind(&inputJSON)
 	if err != nil {
 		return echo.NewHTTPError(http.StatusInternalServerError, "Wrong data")
 	}
 
-	err = db.Conn.Model(&statuses).Where("ID = ?", inputJSON.ID).Select()
+	// Select Worker by id
+	err = db.Conn.Model(&worker).Where("ID = ?", inputJSON.NewWorderID).Select()
 	if err != nil {
-		return echo.NewHTTPError(http.StatusOK, err.Error())
+		return echo.NewHTTPError(http.StatusOK, "Worker not found. "+err.Error())
 	}
+
+	// Select Order by id
+	err = db.Conn.Model(&statuses).Where("ID = ?", inputJSON.OrderID).Select()
+	if err != nil {
+		return echo.NewHTTPError(http.StatusOK, "Order not found. "+err.Error())
+	}
+
+	// Save order
+	SaveOrder(statuses)
 
 	// Get current status
 	if statuses.Status.StatusManufacturing == false {
@@ -35,21 +52,11 @@ func NextStatus(c echo.Context) error {
 		statuses.Status.StatusManufacturing = true
 		statuses.Status.DataManufacturing = time.Now()
 
-		_, err := db.Conn.Model(&statuses).Set("Status = ?", statuses.Status).Where("ID = ?", inputJSON.ID).Update()
-		if err != nil {
-			return echo.NewHTTPError(http.StatusOK, err.Error())
-		}
-
 	} else if statuses.Status.StatusGrinding == false {
 
 		currentStatus = "grinding"
 		statuses.Status.StatusGrinding = true
 		statuses.Status.DataGrinding = time.Now()
-
-		_, err := db.Conn.Model(&statuses).Set("Status = ?", statuses.Status).Where("ID = ?", inputJSON.ID).Update()
-		if err != nil {
-			return echo.NewHTTPError(http.StatusOK, err.Error())
-		}
 
 	} else if statuses.Status.StatusPrinting == false {
 
@@ -57,24 +64,62 @@ func NextStatus(c echo.Context) error {
 		statuses.Status.StatusPrinting = true
 		statuses.Status.DataPrinting = time.Now()
 
-		_, err := db.Conn.Model(&statuses).Set("Status = ?", statuses.Status).Where("ID = ?", inputJSON.ID).Update()
-		if err != nil {
-			return echo.NewHTTPError(http.StatusOK, err.Error())
-		}
-
 	} else {
 
 		currentStatus = "ready"
 		statuses.Status.StatusReady = true
 		statuses.Status.DataReady = time.Now()
 
-		_, err := db.Conn.Model(&statuses).Set("Status = ?", statuses.Status).Where("ID = ?", inputJSON.ID).Update()
-		if err != nil {
-			return echo.NewHTTPError(http.StatusOK, err.Error())
-		}
+	}
 
+	// Change Worker in current order
+	statuses.CurrentWorkerID = worker.ID
+	statuses.CurrentWorkerInitials = worker.Initials
+	statuses.CurrentWorkerPhone = worker.Phone
+
+	// Save current order
+	_, err = db.Conn.Model(&statuses).Set("Status = ?, current_worker_id = ?, current_worker_initials = ?, current_worker_phone = ?", statuses.Status, statuses.CurrentWorkerID, statuses.CurrentWorkerInitials, statuses.CurrentWorkerPhone).Where("ID = ?", inputJSON.OrderID).Update()
+	if err != nil {
+		return echo.NewHTTPError(http.StatusOK, err.Error())
 	}
 
 	return echo.NewHTTPError(http.StatusOK, currentStatus)
 
+}
+
+// SaveOrder - save order before change status
+func SaveOrder(order models.Orders) error {
+
+	err := db.Conn.Insert(&models.SavedOrders{
+		OrderID: order.ID,
+		Title:   order.Title,
+		Date:    order.Date,
+		Status:  order.Status,
+
+		ClientID:       order.ClientID,
+		ClientInitials: order.ClientInitials,
+		ClientPhone:    order.ClientPhone,
+
+		CurrentWorkerID:       order.CurrentWorkerID,
+		CurrentWorkerInitials: order.CurrentWorkerInitials,
+		CurrentWorkerPhone:    order.CurrentWorkerPhone,
+
+		Color:        order.Color,
+		Patina:       order.Patina,
+		FasadArticle: order.FasadArticle,
+		Material:     order.Material,
+
+		CostManufacturing: order.CostManufacturing,
+		CostPainting:      order.CostPainting,
+		CostFinishing:     order.CostFinishing,
+		CostFull:          order.CostFull,
+
+		Params: order.Params,
+	})
+
+	if err != nil {
+		return echo.NewHTTPError(http.StatusOK, err.Error())
+	}
+
+	return echo.NewHTTPError(http.StatusOK, "OK")
 }
