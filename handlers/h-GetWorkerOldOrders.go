82d6df5b8eb28e35b98f12de2f15bf6ea462004b
mrTavas <d.tavasiev1997@gmail.com>
2020-06-18 00:11:15 +0300

    added get oll old worker's orders handler

---
+++ handlers/h-GetWorkerOldOrders.go
@@ -0,0 +1,32 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo"
+
+	// local
+	db "github.com/mrTavas/fw-backend/dbconn"
+)
+
+// GetWorkerOldOrders -
+func GetWorkerOldOrders(c echo.Context) error {
+
+	var OutResponse AllOrders
+	var inputJSON WorkerID
+
+	err := c.Bind(&inputJSON)
+
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, "Wrong data")
+	}
+
+	err = db.Conn.Model(&OutResponse.Orders).Where("current_worker_id = ?", inputJSON.ID).Select()
+
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, err)
+	}
+
+	return echo.NewHTTPError(http.StatusOK, OutResponse)
+
+}
