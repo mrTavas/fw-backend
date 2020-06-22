dc8ee40b1cda0ae15941a2c981073919af5f8128
mrTavas <d.tavasiev1997@gmail.com>
2020-06-23 00:11:28 +0300

    add Get all changes in order handler

---
+++ handlers/h-GetOrderAllChanges.go
@@ -0,0 +1,41 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo"
+
+	// local
+	db "github.com/mrTavas/fw-backend/dbconn"
+	"github.com/mrTavas/fw-backend/models"
+)
+
+// AllOrderChanges - json response type
+type AllOrderChanges struct {
+	Changes []models.OrdersChangesLogs `json:"changes"`
+}
+
+// GetOrderAllChanges -
+func GetOrderAllChanges(c echo.Context) error {
+
+	var inputJSON OrderID
+	var OutResponse AllOrderChanges
+
+	err := c.Bind(&inputJSON)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, "Wrong data")
+	}
+
+	err = db.Conn.Model(&OutResponse.Changes).Where("order_id = ?", inputJSON.ID).Select()
+
+	if err != nil {
+		return echo.NewHTTPError(http.StatusOK, err.Error())
+	}
+
+	for i := 0; i < len(OutResponse.Changes); i++ {
+		OutResponse.Changes[i].Changes = Remove_quotes(OutResponse.Changes[i].Changes)
+	}
+
+	return echo.NewHTTPError(http.StatusOK, OutResponse)
+
+}
