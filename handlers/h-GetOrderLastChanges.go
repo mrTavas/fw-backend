741eee3e269d0e082215130e6c83c4e907123893
mrTavas <d.tavasiev1997@gmail.com>
2020-06-23 00:11:57 +0300

    add Get last changes in order handler

---
+++ handlers/h-GetOrderLastChanges.go
@@ -0,0 +1,49 @@
+package handlers
+
+import (
+	"bytes"
+	"net/http"
+
+	"github.com/labstack/echo"
+
+	// local
+	db "github.com/mrTavas/fw-backend/dbconn"
+	"github.com/mrTavas/fw-backend/models"
+)
+
+// GetOrderLastChanges -
+func GetOrderLastChanges(c echo.Context) error {
+
+	var inputJSON OrderID
+	var OutResponse []models.OrdersChangesLogs
+
+	err := c.Bind(&inputJSON)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, "Wrong data")
+	}
+
+	err = db.Conn.Model(&OutResponse).Where("order_id = ?", inputJSON.ID).Select()
+
+	if err != nil {
+		return echo.NewHTTPError(http.StatusOK, err.Error())
+	}
+
+	if len(OutResponse) == 0 {
+		return echo.NewHTTPError(http.StatusOK, "Нет изменений")
+	}
+
+	OutResponse[len(OutResponse)-1].Changes = Remove_quotes(OutResponse[len(OutResponse)-1].Changes)
+	return echo.NewHTTPError(http.StatusOK, OutResponse[len(OutResponse)-1])
+
+}
+
+func Remove_quotes(s string) string {
+	var b bytes.Buffer
+	for _, r := range s {
+		if r != '"' && r != '\'' {
+			b.WriteRune(r)
+		}
+	}
+
+	return b.String()
+}
