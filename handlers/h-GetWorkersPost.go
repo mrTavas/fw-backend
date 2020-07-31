75329119c391c2cf5b034899854d3909466bb6bd
mrTavas <d.tavasiev1997@gmail.com>
2020-07-31 18:52:33 +0300

    update GetWorkersPost

--- handlers/h-GetWorkersPost.go
+++ handlers/h-GetWorkersPost.go
@@ -7,15 +7,38 @@ import (
 
 	//local
 	db "github.com/mrTavas/fw-backend/dbconn"
+	"github.com/mrTavas/fw-backend/models"
 )
 
-// GetWorkersPost -  Return all managers from db
+// AllWorkersPost - json response type
+type AllWorkersPost struct {
+	Сarpenters []models.Workers `json:"carpenters"`
+	Grinders   []models.Workers `json:"grinders"`
+	Painters   []models.Workers `json:"painters"`
+	Collectors []models.Workers `json:"collectors"`
+}
+
+// GetWorkersPost -  Return all workers with prof from db
 func GetWorkersPost(c echo.Context) error {
 
-	var OutResponse AllWorkers
+	var OutResponse AllWorkersPost
 
-	_, err := db.Conn.Query(&OutResponse.Workerss, "SELECT * FROM workers order by ID")
+	_, err := db.Conn.Query(&OutResponse.Сarpenters, "SELECT * FROM workers WHERE Сarpenter = ?", true)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, err)
+	}
 
+	_, err = db.Conn.Query(&OutResponse.Grinders, "SELECT * FROM workers WHERE Grinder = ?", true)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, err)
+	}
+
+	_, err = db.Conn.Query(&OutResponse.Painters, "SELECT * FROM workers WHERE Painter = ?", true)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, err)
+	}
+
+	_, err = db.Conn.Query(&OutResponse.Collectors, "SELECT * FROM workers WHERE Collector = ?", true)
 	if err != nil {
 		return echo.NewHTTPError(http.StatusInternalServerError, err)
 	}
